34d2d592b237424939634f038ff3bd603e311c02
Daniel Batista <dani_batista20@hotmail.com>
2021-12-15 17:17:42 +0000

    01 BirthdayReminder - add: useState & render peopleList

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const peopleList = data;
+  
+  const [people, setPeople] = useState(peopleList);
+
+  return <main>
+    <section className="container">
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}></List>
+      <button onClick={() => console.log('clicked')}>Clear All</button>
+    </section>
+  </main>;
 }
 
 export default App;
