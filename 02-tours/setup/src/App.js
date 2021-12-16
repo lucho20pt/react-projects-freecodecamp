0b3bf77061d0f141a035b4844e9ba90230a9a93f
Daniel Batista <dani_batista20@hotmail.com>
2021-12-16 18:26:16 +0000

    02 Tours - add: useState & useEffect & fetchToursData

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -1,11 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+// const url = "https://course-api.com/react-tours-project";
+const url = "https://my-json-server.typicode.com/lucho20pt/my-json-server/tours";
+
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  //
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  // fetch data
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const result = await fetch(url);
+      const data = await result.json();
+      console.log(data);
+      setTours(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  // use effect
+  useEffect(() => {
+    fetchTours();
+    return () => {
+      console.log("cleanup");
+    };
+  }, []);
+
+  // loading
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 }
 
-export default App
+export default App;
