bdf22fc2e7678a65bcf6cab273cb22cc3def81c8
Daniel Batista <dani_batista20@hotmail.com>
2021-12-17 13:09:44 +0000

    03 Reviews - add: prev & next Person

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -4,6 +4,32 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
   //
+  const [index, setIndex] = useState(0);
+  const { name, image, job, text } = people[index];
+  // const max = people.length - 1;
+  const checkNum = (number) => {
+    if (number > people.length - 1) return 0;
+    if (number < 0) return people.length - 1;
+    return number;
+  };
+
+  // previous
+  const prevPerson = () => {
+    // index > 0 ? setIndex(index - 1) : setIndex(max);
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNum(newIndex);
+    });
+  };
+
+  // next
+  const nextPerson = () => {
+    // index < max ? setIndex(index + 1) : setIndex(0);
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNum(newIndex);
+    });
+  };
 
   return (
     <article className="review">
@@ -17,10 +43,10 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
