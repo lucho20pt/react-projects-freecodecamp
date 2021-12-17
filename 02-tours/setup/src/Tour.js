a02fc85edd8e663da0be110a54aab554625bfc0e
Daniel Batista <dani_batista20@hotmail.com>
2021-12-17 00:29:11 +0000

    02 Tours - add: readMoreText toggle btn

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
 const Tour = ({ name, image, price, info }) => {
-  // const {name, image, price} = tour
+  //
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -10,7 +12,15 @@ const Tour = ({ name, image, price, info }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">{price}</h4>
         </div>
-        <p>{info}</p>
+
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+
+          <button onClick={ ()=> {setReadMore(!readMore)} }>
+            {readMore ? 'show less' : 'read more'}
+          </button>
+        </p>
+
         <button className="delete-btn" onClick={() => console.log("delete")}>
           not interested
         </button>
