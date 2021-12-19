7e256b0888653f1de1ddc8078f68ec7c2e0725f3
Daniel Batista <dani_batista20@hotmail.com>
2021-12-19 11:29:38 +0000

    04 Accordion - add: iterate listData

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  //
+  // console.log(data);
+  // const list = data;
+  const [list, setList] = useState(data)
+
+  return (
+    <main>
+      <section className="container">
+        <h3>questions and answers about login</h3>
+        <div>
+          {list.map((item) => (
+            <SingleQuestion key={item.id} {...item} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
