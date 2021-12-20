661b2982f51ee0b124795cb3d11e9c6d4ff94e95
Daniel Batista <dani_batista20@hotmail.com>
2021-12-20 15:09:23 +0000

    06 Tabs - add: app display html

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -6,7 +6,40 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://my-json-server.typicode.com/lucho20pt/my-json-server/tabs";
 
 function App() {
-  return <h2>tabs project setup</h2>;
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+
+      <div className="jobs-center">
+
+        {/* btn container */}
+        <div className="btn-container">
+          <button className={"job-btn active-btn"}>item.company</button>
+        </div>
+
+        {/* job info */}
+        <article className="job-info">
+          <h3>title</h3>
+          <h4>company</h4>
+          <p className="job-date">dates</p>
+
+          <div className="job-desc">
+            <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+            <p>jobs</p>
+          </div>
+        </article>
+
+      </div>
+
+      <button type="button" className="btn">
+        more info
+      </button>
+
+    </section>
+  );
 }
 
 export default App;
