2972df392fc68ffaefd559c255c2d84cfa47e0c4
Daniel Batista <dani_batista20@hotmail.com>
2021-12-21 12:07:32 +0000

    07 Slider - add: check conditions for position classes

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -5,8 +5,14 @@ import data from "./data";
 
 function App() {
   //
-  console.log(data)
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
 
+  
+
+  
+
+  // main
   return (
     <section className="section">
       <div className="title">
@@ -15,13 +21,25 @@ function App() {
         </h2>
       </div>
       <div className="section-center">
-        <article>
-          <img src="" alt="" className="person-img" />
-          <h4>{}</h4>
-          <p className="title">{}</p>
-          <p className="text">{}</p>
-          <FaQuoteRight className="icon" />
-        </article>
+        {people.map((person, idx) => {
+          const { id, image, name, quote, title } = person;
+
+          let positon = "nextSlide";
+          if (idx === index) positon = "activeSlide";
+          if (idx === index - 1 || (index === 0 && idx === people.length - 1)) {
+            positon = "lastSlide";
+          }
+
+          return (
+            <article className={`${positon} |${idx}|`} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
 
         <button className="prev">
           <FiChevronLeft />
