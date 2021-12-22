0cbe131ce4e96b777888c3fa095d82c61b5c9f1f
Daniel Batista <dani_batista20@hotmail.com>
2021-12-22 12:35:24 +0000

    08 LoremIpsum - add: submitHandler() & iterate text based on input count

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,17 +1,50 @@
 import React, { useState } from "react";
 import data from "./data";
+
 function App() {
+  //
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  // submit
+  const submitCountHandler = (event) => {
+    event.preventDefault();
+    let amount = parseInt(count);
+    let max = data.length;
+
+    if (amount < 1) amount = 1;
+    if (amount > max) amount = max;
+
+    setText(data.slice(0, amount));
+  };
+
   return (
     <section className="section-center">
       <h3>tired of boring lorem ipsum?</h3>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={submitCountHandler}>
         <label htmlFor="amount">paragraphs:</label>
-        <input type="number" name="amount" id="amount" value="" />
-        <button className="btn">generate</button>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          max={data.length}
+          value={count}
+          onChange={(event) => {
+            setCount(event.target.value);
+          }}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
       </form>
-      <article className="lorem-text">
-        <p>item</p>
-      </article>
+      {text.map((item, index) => {
+        return (
+          <article className="lorem-text" key={index}>
+            <p>{item}</p>
+          </article>
+        );
+      })}
     </section>
   );
 }
