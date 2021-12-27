71b9dcd0fd98de9161a176342d605a9f1835cc3a
Daniel Batista <dani_batista20@hotmail.com>
2021-12-27 19:35:13 +0000

    09 ColorGenerator - add: SingleColor - render html, background, hexColor, and classNames

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,22 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ index, alpha, rgb, type, weight }) => {
+  //
+  const background = rgb.join(",");
+  // console.log(bck);
+  const hexColor = rgbToHex(...rgb);
+  return (
+    <article
+      className={`color ${
+        index > 10 ? "color-light" : null
+      }`}
+      style={{ backgroundColor: `rgb(${background})` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexColor}</p>
+    </article>
+  );
+};
 
-export default SingleColor
+export default SingleColor;
