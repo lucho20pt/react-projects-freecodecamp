c242eee0f204fc9d19b56e9168834e07b0dddfbd
Daniel Batista <dani_batista20@hotmail.com>
2021-12-27 17:19:17 +0000

    09 ColorGenerator - add: submitHandler() && color, error, list useState()

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -5,14 +5,27 @@ import Values from "values.js";
 
 function App() {
   //
-  
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  // submit handler
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(color);
+  };
+
   return (
     <>
       <section className="container">
         <h3>color generator</h3>
-        <form>
+        <form onSubmit={submitHandler}>
           <input
             type="text"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
           />
           <button className="btn" type="submit">
             submit
