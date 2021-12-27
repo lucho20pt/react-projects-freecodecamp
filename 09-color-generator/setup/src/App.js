d1b11557393a60bb941869d70db88b77c7c061fb
Daniel Batista <dani_batista20@hotmail.com>
2021-12-27 18:29:34 +0000

    09 ColorGenerator - add: app - iterate list and return SingleColor && set: setList() & setError()

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -5,14 +5,22 @@ import Values from "values.js";
 
 function App() {
   //
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("#f15025");
   const [error, setError] = useState(false);
   const [list, setList] = useState([]);
 
   // submit handler
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(color);
+    try {
+      let colors = new Values(color).all(10);
+      // console.log(colors);
+      setList(colors);
+      setError(false);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
   };
 
   return (
@@ -21,7 +29,9 @@ function App() {
         <h3>color generator</h3>
         <form onSubmit={submitHandler}>
           <input
+            className={error ? "error" : null}
             type="text"
+            placeholder="#f15025"
             value={color}
             onChange={(e) => {
               setColor(e.target.value);
@@ -32,7 +42,12 @@ function App() {
           </button>
         </form>
       </section>
-      <section className="colors">colors</section>
+      <section className="colors">
+        {list.map((color, index) => {
+          console.log(color)
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
+      </section>
     </>
   );
 }
