6aa02192e8595edec977f978a5560a7af4159a12
Daniel Batista <dani_batista20@hotmail.com>
2021-12-28 16:33:06 +0000

    10 GroceryBud - add: app - set useStates() & onSubmitHandler()

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,21 +3,34 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
+  // states
+  const [name, setName] = useState("");
+  const [list, setList] = useState([{id:123, name: 'item 1'}]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({});
+
+  // submit
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <section className="section-center">
-      <form className="grocery-form">
+      <form className="grocery-form" onSubmit={onSubmitHandler}>
         <Alert />
         <h3>grocery bud</h3>
         <div className="form-control">
           <input type="text" className="grocery" placeholder="e.g. eggs" />
           <button type="submit" className="submit-btn">
-            submit
+            {isEditing ? 'edit' : 'submit'}
           </button>
         </div>
       </form>
 
       <div className="grocery-container">
-        <List />
+        <List items={list} />
         <button className="clear-btn">clear items</button>
       </div>
     </section>
