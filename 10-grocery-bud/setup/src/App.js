e25970d029fc6bf428ac95ecda3fa5c7a9873ef0
Daniel Batista <dani_batista20@hotmail.com>
2021-12-28 15:56:26 +0000

    10 GroceryBud - add: app, alert, list - setup html

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  return (
+    <section className="section-center">
+      <form className="grocery-form">
+        <Alert />
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input type="text" className="grocery" placeholder="e.g. eggs" />
+          <button type="submit" className="submit-btn">
+            submit
+          </button>
+        </div>
+      </form>
+
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,21 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 const List = () => {
-  return <h2>list component</h2>
-}
+  return (
+    <div className="grocery-list">
+      <article className="grocery-item">
+        <p className="title">title</p>
+        <div className="btn-container">
+          <button type="button" className="edit-btn">
+            <FaEdit />
+          </button>
+          <button type="button" className="delete-btn">
+            <FaTrash />
+          </button>
+        </div>
+      </article>
+    </div>
+  );
+};
 
-export default List
+export default List;
